ca58823691c5b498278ebb0b300640e23d6e88e5
Brian Hurst <hursey013@protonmail.com>
2018-03-14 22:43:42 -0400

    Check to see if Pocket is enabled

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-document.body.addEventListener('click',function(e) {
-  if (e.target && e.target.classList.contains('star_img')) {
-    var string = e.target.parentNode.getAttribute('onclick');
-    var id = string.match(/\d+/);
+waitForElement('reader_pane',function() {
+  var pocket = document.querySelector('.article').getAttribute('data-pocket');
 
-    pocket_article(id[0]);
-
-    console.log("Pocket integration triggered.");
+  if (pocket !== '-1') {
+    document.body.addEventListener('click',function(e) {
+      if (e.target && e.target.classList.contains('star_img')) {
+        var string = e.target.parentNode.getAttribute('onclick');
+        var id = string.match(/\d+/);
+    
+        pocket_article(id[0]);
+      }
+    });
   }
 });
+
+function waitForElement(elementId, callBack) {
+  window.setTimeout(function() {
+    var element = document.getElementById(elementId);
+
+    if (element) {
+      callBack(elementId, element);
+    } else {
+      waitForElement(elementId, callBack);
+    }
+  },500)
+}
