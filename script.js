cd83af2f91b32b0e5850707fc7f57c3ef333820e
Brian Hurst <brian.hurst@gsa.gov>
2018-03-15 17:44:34 -0400

    Added icons

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ waitForElement('reader_pane',function() {
       if (e.target && e.target.classList.contains('star_img')) {
         var string = e.target.parentNode.getAttribute('onclick');
         var id = string.match(/\d+/);
-    
+
         pocket_article(id[0]);
       }
     });
